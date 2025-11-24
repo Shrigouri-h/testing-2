559a651d9f118f589f2bdf94f25bee77d604c33a
Shrigouri-h <shrigouri0707@gmail.com>
2025-11-24 16:36:07 +0530

    Update addition.py

--- addition.py
+++ addition.py
@@ -1,17 +1,7 @@
-import sys
-def sum_numbers(x, y):
-    return x + y
-if __name__=="__main__":
-    
-    if len(sys.argv) == 3:
-        script_name = sys.argv[0]
-        a = int(sys.argv[1])
-        b = int(sys.argv[2])
-    else:
-        script_name = sys.argv[0]
-        a = 20
-        b = 30
-    result = sum_numbers(a, b)
-    print("Sum:", result)
+def add(a, b):
+    return a + b
 
-  
+if __name__ == "__main__":
+    x = 10      # parameter 1
+    y = 20      # parameter 2
+    print("Sum:", add(x, y))
